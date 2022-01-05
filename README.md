2d5345d8b6608157ddf4539c6733f0fa768197f1
Jareth Tackett <thoughtspacewebsites@gmail.com>
2022-01-04 20:06:31 -0500

    Beta launch

---
+++ README.md
@@ -0,0 +1,73 @@
+# File Vacuum 5000 #
+
+⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️
+
+**WARNING: This script is designed to manipulate files on both an external drive and another specified mount point on your file system using the super user account. This can have devestating consequences with the wrong config. I am not responsible for any data loss caused by misuse of this tool. Test it out and make sure you know what it's doing and how it works before using it for important data. You have been warned.**
+
+⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️
+
+Import any disk into your computer automatically when it's plugged in. Designed to work with Ubuntu but should work with some other distros too. Written in Node.JS, File Vacuum detects newly plugged in disks, filters through all partitions, mounts them, and then checks for a "sync.json" file in the root of the drive. If the sync.json file is present, it pulls config from the file and beings an rsync of all files on the drive to the computer at a specified location.
+
+Import hard drives, flash drives, or camera SD cards into your computer automatically by just plugging in the device. Email alerts on completion or error. Make your NAS server into a self-feeding file bucket, or install to your laptop for a quick and easy way to import drives.
+
+## Full Usage ##
+
+### Requirements ###
+     - Node.JS
+     - NPM
+     - Yarn (but you could probably use NPM instead...)
+
+MAKE SURE TO BE ROOT. You need root access for this script. It mounts and manipulates drives and filesystems, and root access is the easiest and cleanest way to do this. That means that the script needs to be started with sudo, and that pm2 should run the script as the root user if setting up auto boot.
+
+First, clone the repo:
+
+    git clone https://github.com/jarethmt/File-Vacuum-5000
+
+Then install npm dependencies:
+
+    yarn install
+
+Now, create a file in the main app directory called "mailSettings.json", and fill it in with the following format, substituting your own email details:
+
+    {
+        "from":"SERVER NAME <server@yoururl.com>",
+        "defaultRecipient": "youremail@yoururl.com",
+        "server":{
+            "host": "your.smtp.server.com",
+            "port": 465,
+            "secure": true,
+            "auth": {
+                "user": "your_email",
+                "pass": "your_password"
+            }
+        }
+    }
+
+Start the server with `sudo node .` from the root directory of the project. You can also create a systemd job, or use pm2 in order to run this script in the background at boot. For example:
+
+    sudo su
+    yarn add pm2
+    pm2 start ./index.js
+    pm2 startup
+    pm2 save
+
+Which will use the Node.JS pm2 package to persist this application in the back end.
+
+After everything is installed and configured, test it out by grabbing a flash drive and loading a few sample files on it. Then, create a sync.json file in the root directory of the flash drive with the following format / details:
+
+    {
+        email: your@email.com,
+        syncPath: /home/$USER/last-import,
+        deleteAfter: false,
+        user: uid,
+        group: gid,
+        permissions: xxx
+    }
+
+Where uid is the user ID of the user you'd like the files to be owned by after sync is complete, same for gid. You can obtain these on Ubuntu with `id -u $USER` and `id -g $USER`. Permissions should be a [3 digit binary representation](https://docs.nersc.gov/filesystems/unix-file-permissions/)
+
+Save this file, eject the drive, and then plug it back in, and you should see the script automatically copy everything over to your specified path on the computer! You'll receive a success email on completion. Even if you specify "true" for "deleteAfter", the sync.json will be preserved and left on the drive so that the next time you plug it in, it will sync again! Very useful for creating a physical drop box of sorts.
+
+
+### TODO ###
+ - Create a safety check on import folder. An array of folders defined in the script which can not be synced to, in order to protect sensitive system directories.
---
+++ index.js
@@ -0,0 +1,330 @@
+import { checkSudo } from 'check-sudo'
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+
+var Rsync = require('rsync');
+var drivelist = require("drivelist");
+const nodemailer = require("nodemailer");
+var chmodr = require('chmodr');
+var chownr = require('chownr');
+var _ = require('underscore');
+
+var exec = require('child_process').exec;
+var fs = require('fs');
+
+var mailSettings = require('./mailSettings.json');
+var mailFrom = mailSettings.from;
+var defaultEmail = mailSettings.defaultRecipient
+var mailServer = mailSettings.server
+
+var currentDrives = false;
+
+
+//Check to see if we ran this with sudo, and fail out if not
+checkSudo().then(function(isSudo){
+    if(!isSudo){
+        console.log('You must run this script with sudo');
+        process.exit(1);
+    }
+    else{
+        console.log('Monitoring script starting...');
+    }
+});
+
+
+
+
+//First initialize our mail transport
+let transporter = nodemailer.createTransport(mailServer);
+
+async function logError(error, mailTo){
+    console.log(error);
+    //Then email the user
+    if(!mailTo){
+        mailTo = defaultEmail;
+    }
+    let info = await transporter.sendMail({
+        from: mailFrom,
+        to: mailTo,
+        subject: "File Sync Error",
+        text: "Your file sync encountered an error: "+error,
+      });
+
+      console.log('Error email sent');
+
+}
+
+function startMount(drive) {
+
+    var devPath = drive.device;
+    var escapedPath = devPath.replace(/\//g, '\\/');
+    console.log('starting sync for ' + devPath);
+
+    //First, find all partitions on the disk
+    exec("sudo fdisk -l "+devPath+" | grep -P '"+escapedPath+"\\d' | awk '{print $1}'", function(err, stdout, stderr) {
+        if (err) {
+            logError('error listing disks: ' + err);
+            return;
+        }
+        var partitions = stdout.split('\n').filter(function(part) {return part != ""});
+        
+        console.log('Available disk partitions: ', partitions);
+        console.log('Mounting partitions one by one, and checking for presence of sync file');
+
+
+        //Now mount drives one by one and check for our config file to see if we should sync
+        //First we should see if the drive auto mounted
+        if(drive.mountpoints.length > 0){
+            var mountPoint = drive.mountpoints[0].path;
+            console.log('Drive already mounted at: ' + mountPoint);
+            postMount(mountPoint, drive);
+        } else {
+            for(var i in partitions){
+
+                //Make a temp mount point for our drives
+                const folderName = '/mnt/file-vacuum-'+i;
+                try {
+                    if (!fs.existsSync(folderName)) {
+                        fs.mkdirSync(folderName)
+                    }
+                } catch (err) {
+                    logError(err)
+                }
+
+                exec('sudo mount '+partitions[i]+' '+folderName, function(err, stdout, stderr){
+                    if (err) {
+                        logError('error mounting drive: ' + err);
+
+                        //Remove the folder for the mount point
+                        //fs.rmdirSync(folderName, { recursive: true });
+
+                        return;
+                    }
+
+                    postMount(folderName, drive);
+                    
+                });
+            }
+        }
+
+        //Finally, remove the folder for our mount point
+        
+
+    });
+}
+
+
+function postMount(mountPoint, drive){
+    //Drive is mounted, list the files in it
+    fs.readdir(mountPoint, function(err, files) {
+        if (err) {
+            logError('error listing files: ' + err);
+            return;
+        }
+        //Check if we have our config file
+        if (files.indexOf('sync.json') > -1) {
+            console.log('Found sync.json, syncing...');
+            //We have our config file, get data and set up sync
+            fs.readFile(mountPoint+'/sync.json', async function(err, data) {
+                if (err) {
+                    logError('error reading sync.json: ' + err);
+                    unmountDrive(mountPoint);
+                    return;
+                }
+                var syncData = JSON.parse(data);
+                console.log('Sync data: ', syncData);
+
+                var mailTo = syncData.email;
+                var syncPath = syncData.syncPath;
+                var deleteAfter = syncData.deleteAfter;
+
+                var owner = syncData.owner;
+                var group = syncData.group;
+                var permissions = syncData.permissions;
+
+                if(!mailTo || !syncPath || !owner || !group || !permissions){
+                    logError('Invalid sync data, aborting!');
+                    unmountDrive(mountPoint);
+                    return;
+                }
+
+                //Send an email to the user to let them know we are syncing
+                let info = await transporter.sendMail({
+                    from: mailFrom,
+                    to: mailTo,
+                    subject: "File Sync Starting",
+                    text: "Your drive: "+drive.description+" is being synced to: "+syncPath,
+                  });
+
+                //Make sure our destination directory exists...
+                if (!fs.existsSync(syncPath)){
+                    fs.mkdirSync(syncPath);
+                }
+
+                //Now we have our data, set up the sync
+                var rsync = new Rsync()
+                    .flags('avz')
+                    .source(trailingSlashIt(mountPoint))
+                    .destination(trailingSlashIt(syncPath))
+                    .exclude('sync.json');
+
+                rsync.execute(async function(error, code, cmd) {
+                    if(error){
+                        logError('error syncing: ' + error, mailTo);
+                        unmountDrive(mountPoint, mailTo);
+                        return;
+                    }
+
+                    //Check if we should delete after
+                    if(deleteAfter){
+                        //Recursively delete all files and folders
+                        console.log('Deleting files after sync');
+                        fs.readdir(mountPoint, function(err, files) {
+                            if (err) {
+                                logError('error listing files: ' + err, mailTo);
+                                unmountDrive(mountPoint);
+                                return;
+                            }
+                            for(var i in files){
+                                var file = files[i];
+                                if(file == 'sync.json'){
+                                    continue;
+                                }
+                                console.log("deleting file: ", mountPoint+'/'+file);
+                                fs.rmSync(mountPoint+'/'+file, { recursive: true, force: true });
+                            }
+
+                            correctPermissions(syncPath, owner, group, permissions).then(function(){
+                                unmountDrive(mountPoint, mailTo).then(function(){
+                                    sendSuccessEmail(drive, mailTo);
+                                });
+                            });
+                            
+                        });
+                    }
+                    else{
+                        correctPermissions(syncPath, owner, group, permissions).then(function(){
+                            unmountDrive(mountPoint).then(function(){
+                                sendSuccessEmail(drive);
+                            });
+                        });
+                    }
+                    
+
+                });
+                    
+
+            });
+
+        }
+        else{
+            logError('Drive attached, but no sync.json found, skipping partition');
+            unmountDrive(mountPoint);
+        }
+    });
+
+}
+
+function unmountDrive(mountPoint, mailTo){
+    return new Promise(function(resolve, reject){
+        //End by unmounting this drive...
+        console.log('unmounting drive');
+        exec('umount "'+mountPoint+'"', function(err, stdout, stderr){
+            if(err){
+                logError('error unmounting drive: ' + err, mailTo);
+                reject(err);
+                return;
+            }
+            console.log('drive unmounted successfully');
+            fs.rmdirSync(mountPoint, { recursive: true });
+            resolve();
+        });
+    });
+}
+
+
+async function sendSuccessEmail(drive, mailTo){
+    //Sync completed successfully, lets send an email and unmount the drive
+    console.log('Sync completed successfully!');
+    console.log('Sending email...');
+
+    if(!mailTo){
+        mailTo = defaultEmail;
+    }
+
+    try{
+        //Then email the user
+        let info = await transporter.sendMail({
+            from: mailFrom,
+            to: mailTo,
+            subject: "File Sync Complete",
+            text: "Your file sync of "+drive.description+" has successfully completed! You may now unplug your drive",
+          });
+
+          console.log('Email sent!');
+    }
+    catch(err){
+        logError(err);
+    }
+}
+
+
+function correctPermissions(path, owner, group, permissions){
+    return new Promise(function(resolve, reject){
+        chownr(path, owner, group, function(err){
+            if(err){
+                logError('error changing file owner: ' + err);
+                reject(err);
+                return;
+            }
+            chmodr(path, "0o"+permissions, function(err){
+                if(err){
+                    logError('error changing permissions: ' + err);
+                    reject(err);
+                    return;
+                }
+                resolve();
+            });
+        });
+    });
+}
+
+
+
+
+
+
+
+//Helper functions
+
+
+function untrailingSlashIt(str) {
+    return str.replace(/\/$/, '');
+}
+
+function trailingSlashIt(str) {
+    return untrailingSlashIt(str) + '/';
+}
+
+
+
+
+  //Init our loop watcher
+
+setInterval(async function(){
+    var newDrives = await drivelist.list();
+    if((newDrives.length > currentDrives.length) && currentDrives){
+        var newDrive = _.filter(newDrives, function(obj){ return !_.findWhere(currentDrives, {devicePath: obj.devicePath}); })[0];
+
+        // Now that we've found our drive path, let's trigger a sync
+        startMount(newDrive);
+
+        currentDrives = newDrives;
+    }
+    else if(!currentDrives || newDrives.length < currentDrives.length){
+        //We've detached a drive, lets remove it from our list
+        currentDrives = newDrives
+    }
+}, 5000);
+
